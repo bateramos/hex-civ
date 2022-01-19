90ea627b0c23cb7cdd9e4916d18f85c28b06482a
Pedro Ramos <pedrodevop@gmail.com>
2022-01-19 16:53:15 +0100

    Refactory unit_movement controller

--- src/controllers/unit_movement.rs
+++ src/controllers/unit_movement.rs
@@ -1,106 +1,84 @@
 use sfml::window::*;
 
 use crate::ControlFn;
-use crate::entities::HexagonCategory;
 
 pub fn init_unit_movement() -> ControlFn {
     Box::new(|mut state, _graphics| {
-        if let Some(mut unit) = state.unit_selected {
+        if let Some(unit) = state.unit_selected {
+            let mut x = unit.grid_position.0;
+            let mut y = unit.grid_position.1;
+            let unit_type = state.units[y][x];
+
+            let mut event_triggered = false;
+
             state.events.iter().for_each(|event| {
                 match event {
                     Event::KeyPressed { code: Key::NUMPAD9, .. } => {
-                        let x = unit.grid_position.0;
-                        let y = unit.grid_position.1;
-                        unit.category = HexagonCategory::FIELD;
-                        let unit_type = state.units[y][x];
+                        event_triggered = true;
                         state.units[y][x] = None;
 
-                        let x = if y % 2 == 0 {
+                        x = if y % 2 == 0 {
                             unit.grid_position.0
                         } else {
                             unit.grid_position.0 + 1
                         };
 
-                        let y = y - 1;
-                        state.units[y][x] = unit_type;
-                        state.unit_selected = Some(state.hexagons[y][x]);
+                        y = y - 1;
                     },
                     Event::KeyPressed { code: Key::NUMPAD7, .. } => {
-                        let x = unit.grid_position.0;
-                        let y = unit.grid_position.1;
-                        unit.category = HexagonCategory::FIELD;
-                        let unit_type = state.units[y][x];
+                        event_triggered = true;
                         state.units[y][x] = None;
 
-                        let x = if y % 2 == 0 {
+                        x = if y % 2 == 0 {
                             unit.grid_position.0 - 1
                         } else {
                             unit.grid_position.0
                         };
 
-                        let y = y - 1;
-                        state.units[y][x] = unit_type;
-                        state.unit_selected = Some(state.hexagons[y][x]);
+                        y = y - 1;
                     },
                     Event::KeyPressed { code: Key::NUMPAD4, .. } => {
-                        let x = unit.grid_position.0;
-                        let y = unit.grid_position.1;
-                        unit.category = HexagonCategory::FIELD;
-                        let unit_type = state.units[y][x];
+                        event_triggered = true;
                         state.units[y][x] = None;
 
-                        let x = if y % 2 == 0 {
+                        x = if y % 2 == 0 {
                             unit.grid_position.0 - 1
                         } else {
                             unit.grid_position.0
                         };
 
-                        let y = y + 1;
-                        state.units[y][x] = unit_type;
-                        state.unit_selected = Some(state.hexagons[y][x]);
+                        y = y + 1;
                     },
                     Event::KeyPressed { code: Key::NUMPAD6, .. } => {
-                        let x = unit.grid_position.0;
-                        let y = unit.grid_position.1;
-                        unit.category = HexagonCategory::FIELD;
-                        let unit_type = state.units[y][x];
+                        event_triggered = true;
                         state.units[y][x] = None;
 
-                        let x = if y % 2 == 0 {
+                        x = if y % 2 == 0 {
                             unit.grid_position.0
                         } else {
                             unit.grid_position.0 + 1
                         };
 
-                        let y = y + 1;
-                        state.units[y][x] = unit_type;
-                        state.unit_selected = Some(state.hexagons[y][x]);
+                        y = y + 1;
                     },
                     Event::KeyPressed { code: Key::NUMPAD8, .. } => {
-                        let x = unit.grid_position.0;
-                        let y = unit.grid_position.1;
-                        unit.category = HexagonCategory::FIELD;
-                        let unit_type = state.units[y][x];
+                        event_triggered = true;
                         state.units[y][x] = None;
 
-                        let y = unit.grid_position.1 - 2;
-                        state.units[y][x] = unit_type;
-
-                        state.unit_selected = Some(state.hexagons[y][x]);
+                        y = y - 2;
                     },
                     Event::KeyPressed { code: Key::NUMPAD5, .. } => {
-                        let x = unit.grid_position.0;
-                        let y = unit.grid_position.1;
-                        unit.category = HexagonCategory::FIELD;
-                        let unit_type = state.units[y][x];
+                        event_triggered = true;
                         state.units[y][x] = None;
 
-                        let y = unit.grid_position.1 + 2;
-                        state.units[y][x] = unit_type;
-
-                        state.unit_selected = Some(state.hexagons[y][x]);
+                        y = y + 2;
                     },
                     _ => {}
+                }
+
+                if event_triggered {
+                    state.units[y][x] = unit_type;
+                    state.unit_selected = Some(state.hexagons[y][x]);
                 }
             });
         }
