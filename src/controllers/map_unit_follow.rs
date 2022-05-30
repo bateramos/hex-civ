320e6d112c03db5a32055ec0a1be2e0b408959b2
Pedro Ramos <pedrodevop@gmail.com>
2022-05-30 13:24:24 +0200

    Move camera together with unit movement

--- src/controllers/map_unit_follow.rs
+++ src/controllers/map_unit_follow.rs
@@ -5,14 +5,11 @@ use crate::{ControlEventGraphicFn, UNIT_MOVEMENT_EVENT};
 pub fn init_map_unit_follow<'a>(scale: f32) -> ControlEventGraphicFn<'a> {
     ControlEventGraphicFn {
         event: UNIT_MOVEMENT_EVENT,
-        func: Box::new(move |mut view, state, _graphics, _event| {
-            if let Some(unit_id) = state.unit_selected {
-                let unit = state.units.iter().find(|u| u.id == unit_id).unwrap();
-                let x = unit.position.x;
-                let y = unit.position.y;
+        func: Box::new(move |mut view, state, _graphics, event| {
+            if let Some(_unit_id) = state.unit_selected {
+                let position = event.position.unwrap();
 
-                //view.set_center(Vector2f { x: x as f32 * scale * 40., y: y as f32 * scale * 10. });
-                //println!("{:?}", Vector2f { x: x as f32 * scale, y: y as f32 * scale });
+                view.set_center(Vector2f { x: position.x as f32 * 20. * scale, y: position.y as f32 * 20. * scale });
             }
 
             view
